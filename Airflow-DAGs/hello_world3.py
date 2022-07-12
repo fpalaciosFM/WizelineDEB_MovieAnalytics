075cbff3b132f2373aa577c6530325a400fcf6d5
Fernando Palacios A <42310134+fpalaciosFM@users.noreply.github.com>
2022-07-11 22:16:13 -0500

    Update hello_world3.py

--- Airflow-DAGs/hello_world3.py
+++ Airflow-DAGs/hello_world3.py
@@ -12,7 +12,7 @@ def print_hello():
     return hw
 
 
-dag = DAG('hello_world', description='Hello World DAG',
+dag = DAG('hello_world3', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
