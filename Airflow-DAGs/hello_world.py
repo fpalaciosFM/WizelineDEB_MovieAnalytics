41d2e4cab911ad6c7e6db6994d299228dd6c1ddc
Fernando Palacios <fpalacios.fm@gmail.com>
2022-07-17 10:49:10 -0500

    DAG task log test

--- Airflow-DAGs/hello_world.py
+++ Airflow-DAGs/hello_world.py
@@ -5,19 +5,29 @@ from airflow.operators.python_operator import PythonOperator
 
 
 def print_hello():
-    hw = 'Hello world from your first Airflow DAG!'
+    hw = "Hello world from your first Airflow DAG!"
 
-    with open('test_airflow.txt', 'w') as f:
-        f.write('hola mundo')
+    with open("test_airflow.txt", "w") as f:
+        f.write("hola mundo")
+
+    print("Hola Log")
+
+    with open("test_airflow.txt", "r") as f:
+        print(f.readlines)
 
     return hw
 
 
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+dag = DAG(
+    "hello_world",
+    description="Hello World DAG",
+    schedule_interval="0 12 * * *",
+    start_date=datetime(2017, 3, 20),
+    catchup=False,
+)
 
 hello_operator = PythonOperator(
-    task_id='hello_task', python_callable=print_hello, dag=dag)
+    task_id="hello_task", python_callable=print_hello, dag=dag
+)
 
 hello_operator
