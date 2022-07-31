157a2dfe4c3d1e3e4db6c062f9b8923027feed0b
Fernando Palacios <fpalacios.fm@gmail.com>
2022-07-30 19:51:17 -0500

    update movie_review script with comments

--- Dataproc/movie_review_identify_positives.py
+++ Dataproc/movie_review_identify_positives.py
@@ -1,29 +1,62 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.ml.feature import Tokenizer, StopWordsRemover
-from pyspark.sql.functions import col, filter, array, lit, regexp_replace, array_distinct, arrays_overlap, when
+from pyspark.sql.functions import (
+    col,
+    array,
+    lit,
+    regexp_replace,
+    array_distinct,
+    arrays_overlap,
+    when,
+)
 
-spark = SparkSession \
-       .builder \
-       .appName("WDEB") \
-       .getOrCreate()
+spark = SparkSession.builder.appName("WDEB").getOrCreate()
 
-df = spark.read.csv('gs://wizeline-deb-movie-analytics-fpa/movie_review.csv', inferSchema="true", header="true")
-df = df.withColumn('review_str', regexp_replace(col('review_str'), '([^a-zA-Z0-9 ])', ' $1 '))
+# read movie_review.csv file into a dataframe
+df = spark.read.csv(
+    "gs://wizeline-deb-movie-analytics-fpa/movie_review.csv",
+    inferSchema="true",
+    header="true",
+)
 
+# delete non alphanumeric characters from column review_str
+df = df.withColumn(
+    "review_str", regexp_replace(col("review_str"), "([^a-zA-Z0-9 ])", " $1 ")
+)
+
+# apply tokenizer to column review_str to split the reviews into an array of words
 df = Tokenizer(inputCol="review_str", outputCol="tokens").transform(df)
-df = df.withColumn('tokens', array_distinct(col('tokens')))
 
+# remove duplicate words
+df = df.withColumn("tokens", array_distinct(col("tokens")))
+
+# array of stop words
 stop_words = []
-df = StopWordsRemover(stopWords=stop_words, inputCol='tokens', outputCol='tokens2').transform(df)
 
-positive_words = ['good', 'great', 'excelent']
-f_is_positive = lambda x: 1 if x in positive_words else 0
+# remove stop words from column tokens
+df = StopWordsRemover(
+    stopWords=stop_words, inputCol="tokens", outputCol="tokens2"
+).transform(df)
 
-df = df.withColumn('positive_words', array([lit(i) for i in positive_words]))
-df = df.withColumn('positive_review', arrays_overlap(col('tokens2'), col('positive_words')))
+# array of positive words
+positive_words = ["good", "great", "excelent"]
 
-df = df.drop('tokens2').drop('tokens').drop('review_str').drop('positive_words')
+# create a new column with contant value the content of the positive_words array
+df = df.withColumn("positive_words", array([lit(i) for i in positive_words]))
 
-df = df.withColumn('positive_review', when(df.positive_review, 1).otherwise(0))
+# check for every row if the arrays in columns tokens2 and positive_words contain at least one word in common
+df = df.withColumn(
+    "positive_review", arrays_overlap(col("tokens2"), col("positive_words"))
+)
 
-df.write.parquet('gs://wizeline-deb-movie-analytics-fpa/parquet/movie_review.parquet', mode='overwrite')
+# removal of auxiliary columns
+df = df.drop("tokens2").drop("tokens").drop("review_str").drop("positive_words")
+
+# replacement of data type in column positive_review, from boolean to integer
+df = df.withColumn("positive_review", when(df.positive_review, 1).otherwise(0))
+
+# save file in bucket with parquet format
+df.write.parquet(
+    "gs://wizeline-deb-movie-analytics-fpa/parquet/movie_review.parquet",
+    mode="overwrite",
+)
