9a30555b32fdd1cc9ad25c9e45d8b615be32e390
Fernando Palacios <fpalacios.fm@gmail.com>
2022-08-01 16:33:46 -0500

    dataproc script for log_review transformation

---
+++ Dataproc/log_review_expand_xml.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("WDEB").getOrCreate()
+
+# read csv from gcs
+df = spark.read.csv(
+    "gs://wizeline-deb-movie-analytics-fpa/log_reviews.csv",
+    inferSchema="true",
+    header="true",
+)
+
+# expand xml column
+df = df.selectExpr(
+    "id_review",
+    *[
+        f"xpath(log, 'reviewlog/log/{col}/text()')[0] as {col}"
+        for col in ["logDate", "device", "location", "os", "ipAddress", "phoneNumber"]
+    ],
+)
+
+# rename columns
+df = (
+    df.withColumn("log_id", col("id_review"))
+    .withColumn("log_date", col("logDate"))
+    .withColumn("ip", col("ipAddress"))
+    .withColumn("phone_number", col("phoneNumber"))
+    .withColumn("browser", col("os"))
+)
+
+# select required columns
+df = df.select(
+    "log_id", "log_date", "device", "os", "location", "browser", "ip", "phone_number"
+)
