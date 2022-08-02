b0d837d27e3e177ab6bb1e66ad883d73a633ebf5
Fernando Palacios <fpalacios.fm@gmail.com>
2022-08-01 23:11:44 -0500

    log_review transformation dag

---
+++ Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_TransformLogReview.py
@@ -0,0 +1,46 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocInstantiateWorkflowTemplateOperator,
+)
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCreateExternalTableOperator,
+)
+
+default_args = {
+    "owner": "Fernando Palacios",
+    "depends_on_past": False,
+    "start_date": datetime(2021, 10, 1),
+    "email": ["fpalacios.fm.gcp@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+dag = DAG(
+    "WDEB_CP_MovieAnalytics_ETL_TransformLogReview",
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+)
+
+task_expand_column_ = DataprocInstantiateWorkflowTemplateOperator(
+    dag=dag,
+    task_id="identify_positive_reviews",
+    gcp_conn_id="google_cloud_default",
+    template_id="wdeb-template-movie-review",
+    region="us-west1",
+)
+
+task_bigquery_create_external_table = BigQueryCreateExternalTableOperator(
+    dag=dag,
+    task_id="bigquery_create_external_table",
+    bucket="wizeline-deb-movie-analytics-fpa",
+    source_objects=["parquet/log_review.parquet/*.parquet"],
+    destination_project_dataset_table="stg.log_review",
+    source_format="parquet",
+    google_cloud_storage_conn_id="google_cloud_default",
+    location="us-west1",
+)
