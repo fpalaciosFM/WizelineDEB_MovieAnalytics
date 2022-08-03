0f083f2639f6227de5e7ae1f49bd6e816e0db6f6
Fernando Palacios <fpalacios.fm@gmail.com>
2022-08-03 02:01:55 -0500

    user_purchase extraction dag creation

---
+++ Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_TransformUserPurchase.py
@@ -0,0 +1,36 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import (
+    PostgresToGCSOperator,
+)
+
+default_args = {
+    "owner": "Fernando Palacios",
+    "depends_on_past": False,
+    "start_date": datetime(2021, 10, 1),
+    "email": ["fpalacios.fm.gcp@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+dag = DAG(
+    "WDEB_CP_MovieAnalytics_ETL_PrepareInitialState",
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+)
+
+task_extract_postgres_to_csv_gcs_user_purchase = PostgresToGCSOperator(
+    dag=dag,
+    task_id="extract_postgres_to_csv_gcs_user_purchase",
+    postgres_conn_id="postgres_default",
+    query="SELECT * FROM stg.user_purchase",
+    bucket="wizeline-deb-movie-analytics-fpa",
+    filename="user_purchase.csv",
+)
+
+task_extract_postgres_to_csv_gcs_user_purchase
