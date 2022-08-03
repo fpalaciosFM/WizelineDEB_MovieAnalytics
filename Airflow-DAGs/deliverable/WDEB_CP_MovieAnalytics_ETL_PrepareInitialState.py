130966c47d9cc21ef683f8fb18dba410623392f4
Fernando Palacios <fpalacios.fm@gmail.com>
2022-08-03 02:53:37 -0500

    trigger run user_purchase transformation dag

--- Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_PrepareInitialState.py
+++ Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_PrepareInitialState.py
@@ -166,9 +166,16 @@ trigger_run_log_preview_transformation_dag = TriggerDagRunOperator(
     trigger_dag_id="WDEB_CP_MovieAnalytics_ETL_TransformLogReview",
 )
 
+trigger_run_user_purchase_transformation_dag = TriggerDagRunOperator(
+    dag=dag,
+    task_id="run_user_purchase_transformation_dag",
+    trigger_dag_id="WDEB_CP_MovieAnalytics_ETL_TransformUserPurchase",
+)
+
 task_load_gdrive_to_gcs_movie_review >> trigger_run_movie_preview_transformation_dag
 task_load_gdrive_to_gcs_log_reviews >> trigger_run_log_preview_transformation_dag
 (
     task_postgres_create_table_user_purchase
     >> task_load_gdrive_csv_to_postgres_user_purchase
+    >> trigger_run_user_purchase_transformation_dag
 )
