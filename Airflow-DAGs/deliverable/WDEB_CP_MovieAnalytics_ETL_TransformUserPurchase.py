af3f59df88375bebda7855716a9e0119e7571692
Fernando Palacios <fpalacios.fm@gmail.com>
2022-08-03 02:37:47 -0500

    user_purchase dag test

--- Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_TransformUserPurchase.py
+++ Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_TransformUserPurchase.py
@@ -1,9 +1,14 @@
 from datetime import timedelta, datetime
 
 from airflow import DAG
-from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import (
     PostgresToGCSOperator,
+)
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocInstantiateWorkflowTemplateOperator,
+)
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCreateExternalTableOperator,
 )
 
 default_args = {
@@ -24,13 +29,36 @@ dag = DAG(
     catchup=False,
 )
 
-task_extract_postgres_to_csv_gcs_user_purchase = PostgresToGCSOperator(
+task_extract_postgres_to_json_gcs_user_purchase = PostgresToGCSOperator(
     dag=dag,
-    task_id="extract_postgres_to_csv_gcs_user_purchase",
+    task_id="extract_postgres_to_json_gcs_user_purchase",
     postgres_conn_id="postgres_default",
     sql="SELECT * FROM stg.user_purchase",
     bucket="wizeline-deb-movie-analytics-fpa",
-    filename="user_purchase.csv",
+    filename="user_purchase.json",
 )
 
-task_extract_postgres_to_csv_gcs_user_purchase
+task_convert_json_to_parquet = DataprocInstantiateWorkflowTemplateOperator(
+    dag=dag,
+    task_id="convert_json_to_parquet",
+    gcp_conn_id="google_cloud_default",
+    template_id="wdeb-template-user-purchase",
+    region="us-west1",
+)
+
+task_bigquery_create_external_table = BigQueryCreateExternalTableOperator(
+    dag=dag,
+    task_id="bigquery_create_external_table",
+    bucket="wizeline-deb-movie-analytics-fpa",
+    source_objects=["parquet/user_purchase.parquet/*.parquet"],
+    destination_project_dataset_table="stg.user_purchase",
+    source_format="parquet",
+    google_cloud_storage_conn_id="google_cloud_default",
+    location="us-west1",
+)
+
+(
+    task_extract_postgres_to_json_gcs_user_purchase
+    >> task_convert_json_to_parquet
+    >> task_bigquery_create_external_table
+)
