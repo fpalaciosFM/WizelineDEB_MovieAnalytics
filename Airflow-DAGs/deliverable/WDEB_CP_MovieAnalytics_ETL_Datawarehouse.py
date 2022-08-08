ba0c06126b381f44f67f5c3f8d6064427595edaf
Fernando Palacios <fpalacios.fm@gmail.com>
2022-08-08 10:19:52 -0500

    dag fact table test

--- Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_Datawarehouse.py
+++ Airflow-DAGs/deliverable/WDEB_CP_MovieAnalytics_ETL_Datawarehouse.py
@@ -33,6 +33,20 @@ sensor_wait_for_transform_log_review = ExternalTaskSensor(
     external_task_id="bigquery_create_external_table",
 )
 
+sensor_wait_for_transform_movie_review = ExternalTaskSensor(
+    dag=dag,
+    task_id="wait_for_transform_movie_review",
+    external_dag_id="WDEB_CP_MovieAnalytics_ETL_TransformMovieReview",
+    external_task_id="bigquery_create_external_table",
+)
+
+sensor_wait_for_transform_user_purchase = ExternalTaskSensor(
+    dag=dag,
+    task_id="wait_for_transform_user_purchase",
+    external_dag_id="WDEB_CP_MovieAnalytics_ETL_TransformUserPurchase",
+    external_task_id="bigquery_create_external_table",
+)
+
 task_dim_location_create = BigQueryExecuteQueryOperator(
     dag=dag,
     task_id="dim_location_create",
@@ -133,8 +147,84 @@ task_dim_os_create = BigQueryExecuteQueryOperator(
     """,
 )
 
+task_fact_movie_analytics = BigQueryExecuteQueryOperator(
+    dag=dag,
+    task_id="fact_movie_analytics",
+    sql=""""
+        DROP TABLE IF EXISTS
+        dw.id_fact_movie_analytics;
+        CREATE TABLE
+        dw.fact_movie_analytics AS(
+        WITH
+            amount_collected_per_customer AS (
+            SELECT
+            customer_id,
+            SUM(quantity * unit_price) AS amount_collected
+            FROM
+            `stg.user_purchase`
+            GROUP BY
+            customer_id )
+        SELECT
+            ROW_NUMBER() OVER () AS id_fact_movie_analytics,
+            DIM_DATE.id_dim_date,
+            DIM_LOC.id_dim_location,
+            DIM_DEV.id_dim_device,
+            DIM_OS.id_dim_os,
+            DIM_BRO.id_dim_browser,
+            SUM(AC.amount_collected) AS amount_collected,
+            SUM(MR.positive_review) AS review_score,
+            COUNT(MR.id_review) AS review_count,
+            CURRENT_TIMESTAMP() AS insert_date
+        FROM
+            `stg.log_review` AS LR
+        LEFT JOIN
+            `dw.dim_location` AS DIM_LOC
+        ON
+            LR.location = DIM_LOC.location
+        LEFT JOIN
+            `dw.dim_devices` AS DIM_DEV
+        ON
+            LR.device = DIM_DEV.device
+        LEFT JOIN
+            `dw.dim_os` AS DIM_OS
+        ON
+            LR.os = DIM_OS.os
+        LEFT JOIN
+            `dw.dim_browser` AS DIM_BRO
+        ON
+            LR.browser = DIM_BRO.browser
+        LEFT JOIN
+            `stg.movie_review` AS MR
+        ON
+            LR.log_id = MR.id_review
+        LEFT JOIN
+            amount_collected_per_customer AS AC
+        ON
+            AC.customer_id = MR.cid
+        LEFT JOIN
+            `dw.dim_date` AS DIM_DATE
+        ON
+            LR.log_date = DIM_DATE.log_date
+        GROUP BY
+            DIM_LOC.id_dim_location,
+            DIM_DEV.id_dim_device,
+            DIM_OS.id_dim_os,
+            DIM_BRO.id_dim_browser,
+            DIM_DATE.id_dim_date )
+    """,
+)
+
 sensor_wait_for_transform_log_review >> task_dim_location_create
 sensor_wait_for_transform_log_review >> task_dim_browser_create
 sensor_wait_for_transform_log_review >> task_dim_date_create
 sensor_wait_for_transform_log_review >> task_dim_devices_create
 sensor_wait_for_transform_log_review >> task_dim_os_create
+
+sensor_wait_for_transform_movie_review >> task_fact_movie_analytics
+sensor_wait_for_transform_user_purchase >> task_fact_movie_analytics
+
+task_dim_location_create >> task_fact_movie_analytics
+task_dim_browser_create >> task_fact_movie_analytics
+task_dim_date_create >> task_fact_movie_analytics
+task_dim_devices_create >> task_fact_movie_analytics
+task_dim_os_create >> task_fact_movie_analytics
